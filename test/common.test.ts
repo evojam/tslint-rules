3299492f7fd26b570a2866416eca83eb4d053b28
Krystian Pudlik <kpudlik@evojam.com>
2017-07-19 02:31:47 +0200

    Add rules - TypeScript-specific and Functionality

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,8 +1,8 @@
 // adjacent-overload-signatures
 interface IAdjacentOverloadSignatures {
-  a()
-  b()
-  a(x: string)
+  a(): void
+  b(): void
+  a(x: string): void
 }
 
 // ban-types
@@ -12,33 +12,34 @@ const bool: Boolean
 const num: Number
 const obj: Object
 const str: String
+const fn: Function
 
 // semicolon
-const semicolon = 1;
+const semicolon: number = 1;
 
 // member-access
 class MemberAccess {
   constructor() {}
-  get a() {}
-  get b() {}
-  method() {}
+  get a(): void {}
+  get b(): void {}
+  method(): void {}
 }
 
 // member-ordering
 class MemberOrdering {
   constructor() {}
-  private propa
-  public propb
-  protected propc
-  public static propsa
-  private static propsb
-  protected static propsc
-  private methodb() {}
-  public methoda() {}
-  protected methodc() {}
-  public static methodsa() {}
-  private static methodsb() {}
-  protected static methodsc() {}
+  private propa: string
+  public propb: string
+  protected propc: string
+  public static propsa: string
+  private static propsb: string
+  protected static propsc: string
+  private methodb(): void {}
+  public methoda(): void {}
+  protected methodc(): void {}
+  public static methodsa(): void {}
+  private static methodsb(): void {}
+  protected static methodsc(): void {}
 }
 
 // no-any
@@ -49,3 +50,9 @@ import "./helpers/side-effect-import"
 import "rxjs/add/operator/filter"
 import "rxjs/add/observable/bindCallback"
 import "rxjs"
+
+// typedef
+const untypedFn = () => 'foo'
+function callback(callbackHandler: Function): void {}
+callback(() => 'foo')
+
