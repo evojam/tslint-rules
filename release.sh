3bd7cf17b433bd29b531278a37d2d250644688ac
Krystian Pudlik <kpudlik@evojam.com>
2017-10-04 17:34:30 +0200

    Add empty line, remove redundant #

--- release.sh
+++ release.sh
@@ -5,7 +5,6 @@ set -e
 CURRENT_VERSION=$(grep '"version":' ./package.json | cut -d\" -f4)
 SEMVER_REGEX=^[0-9]+\.[0-9]+\.[0-9]+
 
-
 echo "Current version is set to $CURRENT_VERSION"
 echo ""
 echo "Please enter new version number by using semantic versioning: MAJOR.MINOR.PATCH, where:"
@@ -19,16 +18,18 @@ if ! [[ ${NEW_VERSION} =~ ${SEMVER_REGEX} ]]; then
   echo "Provided version doesn't match MAJOR.MINOR.PATCH format!"
   exit 1
 fi
-#
+
+RELEASE_BRANCH=release/${NEW_VERSION}
+
 git checkout develop
 git pull
 
-git checkout -b release/${NEW_VERSION}
+git checkout -b RELEASE_BRANCH
 npm version ${NEW_VERSION} --no-git-tag-version
 git add .
 git commit -m ${NEW_VERSION}
-git push --set-upstream origin release/${NEW_VERSION}
+git push --set-upstream origin ${RELEASE_BRANCH}
 
-echo "Branch release/${NEW_VERSION} has been created and pushed."
+echo "Branch ${RELEASE_BRANCH} has been created and pushed."
 
 exit
